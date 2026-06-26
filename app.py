e7f584dd703797916b006f292068a6b4f771645f
Swetha <swetha.cs2006@gmail.com>
2026-06-26 15:02:43 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/api/greeting')
+def greeting():
+        return jsonify({"message": "Welcome to my portfolio",
+                        "name": "swetha",
+                        "role": "webdeveloper"})
 if __name__ == '__main__':
     app.run(debug=True)
